0c86c1296abdbd430519df3555302df390a3b50a
namoktin <matkhau12012@gmail.com>
2024-10-20 18:22:58 +0700

    init project

---
+++ dangnhap.js
@@ -0,0 +1,15 @@
+var button = document.querySelector('.buttonn');
+var tat = document.querySelector('.tat');
+var khungngoai = document.querySelector('.khungngoai');
+button.onclick = ()=>{
+	khungngoai.classList.remove('an');
+}
+tat.onclick = ()=>{
+	khungngoai.style.animation = 'andi 0.7s ease forwards';
+	setTimeout(()=>{
+		khungngoai.classList.add('an');
+		khungngoai.style.animation = 'hienra 1s ease';
+	}
+	,700);
+}
+import chichi from './song/modul.js'
